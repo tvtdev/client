0b67660868b56271c0a501401e433b613e161dd3
hmm <asok00000@qq.com>
2019-01-16 23:31:57 +0800

    update execCommand signal

--- cmdthread.h
+++ cmdthread.h
@@ -4,18 +4,26 @@
 #include <QDebug>
 #include <QTextStream>
 #include <QTimer>
+
+#include <td/telegram/Client.h>
+#include <td/telegram/Log.h>
+#include <td/telegram/td_api.h>
+#include <td/telegram/td_api.hpp>
+namespace td_api = td::td_api;
+using Object = td_api::object_ptr<td_api::Object>;
+using Handler = std::function<void(Object)>;
 class CmdThread : public QThread
 {
 	Q_OBJECT
 
-public:
+  public:
 	CmdThread(QObject *parent = nullptr);
 	~CmdThread();
-signals:
-	void execCommand(QString command);
-public slots:
+  signals:
+	void execCommand(QString command,Handler handler={});
+  public slots:
 	void getCommand();
-protected:
-	virtual void run() override;
 
+  protected:
+	virtual void run() override;
 };
